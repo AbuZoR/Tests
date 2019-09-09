8e9c30cabc922f3d05b4c4b282470bfc0ee926ec
AbuZoR <55107967+AbuZoR@users.noreply.github.com>
2019-09-09 21:09:15 +0200

    Add files for test
    Animal Test

---
+++ Test/Test/Animal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Xml;
+
+namespace Test
+{
+    public abstract class Animal
+    {
+        public int legs;
+        public string noise;
+
+        public int Legs
+        {
+            get { return legs; }
+            set { legs = value; }
+        }
+
+        public string MakeNoise
+        {
+            get { return noise; }
+            set { noise = value; }
+        }
+
+        public string ToXML(object obj)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlDeclaration xmlDec = xmlDoc.CreateXmlDeclaration("1.0", "utf-8", string.Empty);
+            xmlDoc.PrependChild(xmlDec);
+
+            XmlElement xmlEle = xmlDoc.CreateElement($"{obj.GetType().Name}");
+            xmlDoc.AppendChild(xmlEle);
+
+            XmlElement elem = null;
+            Type idType = obj.GetType();
+
+            foreach (PropertyInfo pInfo in idType.GetProperties())
+            {
+                object o = pInfo.GetValue(obj, null);
+                elem = xmlDoc.CreateElement(pInfo.Name);
+                elem.InnerText = o.ToString();
+                xmlEle.AppendChild(elem);
+            }
+
+            return xmlDoc.OuterXml;
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog dog = new Dog
+            {
+                Legs = 4,
+                MakeNoise = "Woof"
+            };
+
+            Duck duck = new Duck
+            {
+                Legs = 2,
+                MakeNoise = "Quack"
+            };
+
+            Console.WriteLine($"A {dog.GetType().Name} has {dog.legs} legs and says {dog.noise}");
+            Console.WriteLine($"A {duck.GetType().Name} has {duck.legs} legs and says {duck.noise}");
+            Console.WriteLine($"{dog.ToXML(dog)}");
+            Console.WriteLine($"{duck.ToXML(duck)}");
+            Console.ReadLine();
+        }
+    }
+}
